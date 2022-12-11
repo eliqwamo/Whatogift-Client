9c06b5f4e3912de2c088d076c177ea05996f09ed
Elihu Chitrit <eli@qwamo.com>
2022-12-11 16:01:03 +0200

    11122022

--- whatogift/src/screens/dashboard/index.js
+++ whatogift/src/screens/dashboard/index.js
@@ -4,11 +4,16 @@ import Style from '../../utilis/AppStyle';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Colors from '../../utilis/AppColors';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../../../store/actions';
+import * as Location from 'expo-location';
 
 
 const Dashboard = (props) => {
 
+    const dispatch = useDispatch();
     const [token, setToken] = useState('');
+    const [location, setLocation] = useState(null);
 
     const getDataFromAsync = useCallback(async ()=> {
         const dataFromAsync = await AsyncStorage.getItem('Account');
@@ -23,12 +28,36 @@ const Dashboard = (props) => {
     },[getDataFromAsync])
 
 
-    console.log(token);
 
+    useEffect(() => {
+        (async () => {
+          
+          let { status } = await Location.requestForegroundPermissionsAsync();
+          if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+          }
+    
+          let location = await Location.getCurrentPositionAsync({});
+          setLocation(location);
+
+          if(token && location){
+              console.log(location);
+            const action = actions.getOverview(token,location);
+            try {
+                dispatch(action);
+                setIsLoading(false);
+            } catch (error) {
+                
+            }
+          }
+
+        })();
+      }, []);
     return(
         <View style={Style.container}>
             <Text>Dashboard</Text>
-            <Button onPress={() => {props.navigation.navigate('test')}} title='Go TO TEST' />
+            <Button onPress={() => {dispatch(actions.logout())}} title='Logout' />
         </View>
     )
 }
--- whatogift/store/actions.js
+++ whatogift/store/actions.js
@@ -1,16 +1,60 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const GET_OVERVIEW = 'GET_OVERVIEW';
 
+export const logout = () => {
+    AsyncStorage.removeItem('Account');
+    return { type:LOGOUT }
+} 
 
 export const loginDispatch = (data) => {
     return dispatch => {
         dispatch({ type: LOGIN, data: data })
     }
 }
+
+
+export const getOverviewDispatch = (data) => {
+    return dispatch => {
+        dispatch({ type: GET_OVERVIEW, data: data })
+    }
+}
+export const getOverview = (token, location) => {
+    return async dispatch => {
+        try {
+            const url = 'http://10.100.6.1:3001/api/company/get_companies_by_location';
+            const request = await fetch(url, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    latitude: location.coords.latitude,
+                    longtitude: location.coords.longitude
+                })
+            })
+            const data = await request.json();
+
+            if(data.status){
+                console.log(JSON.stringify(data));
+                dispatch(getOverviewDispatch(data.message))
+            } else {
+                let message = data.message;
+                throw new Error(message);
+            }
+        } catch (error) {
+            console.log('ERRRRRR: ' + JSON.stringify(error));
+        }
+    }
+}
+
+
 export const login = (email,password) => {
     return async dispatch => {
         try {
-            const url = 'http://10.100.8.1:3001/api/account/login';
+            const url = 'http://10.100.6.1:3001/api/account/login';
             const request = await fetch(url, {
                 method: 'post',
                 headers: {'Content-Type': 'application/json'},
@@ -29,7 +73,7 @@ export const login = (email,password) => {
                     email: data.message.email,
                     avatar: data.message.avatar
                 }));
-                dispatch(loginDispatch(data))
+                dispatch(loginDispatch(data.message))
             } else {
                 let message = data.message;
                 throw new Error(message);
