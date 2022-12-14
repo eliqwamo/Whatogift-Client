d8224418d8997161733eae003c9823e0108f8143
Elihu Chitrit <eli@qwamo.com>
2022-12-14 09:49:22 +0200

    14122022

--- whatogift/src/utilis/AppStyle.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-import Colors from './AppColors';
-
-export default StyleSheet.create({
-    container:{
-        flex:1, backgroundColor:Colors.bg,
-        padding:30
-    }
-})
